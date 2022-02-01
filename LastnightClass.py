1f03551c6c1fcd917cd3801605e31d8a6a585ec6
Nathan Hood <nathanjhood@googlemail.com>
2022-02-01 04:37:45 +0100

    Continue intial "For" loop

--- LastnightClass.py
+++ LastnightClass.py
@@ -12,14 +12,17 @@ print("ABOUT LAST NIGHT... (v0.01)")
 print("\n")
 print("*** DISCLAIMER - this application can only provide you with approximations, and should not be taken as medical guidance.\nPlease consult your GP for more information about alcohol consumption. ***")
 print("\n")
-print("Select drink.")
-print("\n")
-print("1.Beer")
-print("2.Wine")
-print("3.Liquor")
-print("\n")
 
 while True:
+
+    print("Select drink.")
+    print("\n")
+    print("1.Beer")
+    print("2.Wine")
+    print("3.Liquor")
+    print("\n")
+
+
     choice = input("Enter Choice(1/2/3): ")
 
     if choice in ('1', '2', '3'):
@@ -40,6 +43,10 @@ while True:
         elif choice == '3':
             print("\n")
             print("You drank a total of", round(amt, 2), "servings of", round(abv, 2), "% liquor, at", round(ml, 2), "per serving;\nYou drank a total of", round(unt, 2), "units of alcohol.\nYou drank a total of", ml * amt, "ml of this beverage.\nYou spent a total of", prc * amt, "on this beverage.")
+
+    else:
+        print("Invalid Input")
+        continue
 
         next_calculation = input("\nDo you want to calculate another drink? (yes/no): ")
 
